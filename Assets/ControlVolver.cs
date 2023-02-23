1fb339844f56e880c427a0595fce720066c4b8d1
Noxius (Juanes) <juancho09mg@gmail.com>
2023-02-23 09:46:13 -0500

    Creacion de Menu y Creditos
    Falta terminar de implementar los botones y el nivel

---
+++ Assets/ControlVolver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlVolver : MonoBehaviour
+{
+    public void OnPressVolver()
+    {
+        Debug.Log("Volver");
+    }
+}

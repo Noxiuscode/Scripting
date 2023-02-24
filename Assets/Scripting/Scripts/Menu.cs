ab0e55c36ae1fca6f9c9c0fa4c53727743d39429
Noxius (Juanes) <juancho09mg@gmail.com>
2023-02-24 01:13:38 -0500

    Navegacion
    Navegacion entre secciones finalizada

---
+++ Assets/Scripting/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void OnPressJugar()
+    {
+        SceneManager.LoadScene("Nivel1", LoadSceneMode.Single);
+
+    }
+
+    public void OnPressInstrucciones()
+    {
+        Debug.Log("Instrucciones");
+        SceneManager.LoadScene("Instrucciones",LoadSceneMode.Single);
+
+    }
+
+    public void OnPressCreditos()
+    {
+        SceneManager.LoadScene("Creditos", LoadSceneMode.Single);
+    }
+
+    public void OnPressControles()
+    {
+        SceneManager.LoadScene("Controles", LoadSceneMode.Single);
+    }
+}

b074049935d8dbebb9a2e7b4783dba56dcf7b512
TheArchess <93046247+SaraL029@users.noreply.github.com>
2023-03-03 08:44:46 -0500

    Creacion de contador de gatitos
    puntaje

---
+++ Assets/Scripting/Scripts/ControlGatitos.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControlGatitos : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txt_contador_gatitos;
+    private int contador_gatitos;
+
+    public void ActualizarGatitos(int valor)
+    {
+        contador_gatitos += valor;
+        ActualizarValorUI();
+    }
+    public void ActualizarValorUI()
+    {
+        txt_contador_gatitos.text = "" + contador_gatitos;
+    }
+}
---
+++ Assets/Scripting/Scripts/Gatito.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+
+public class Gatito : PickableItem
+{
+    [SerializeField]
+    private GameObject ui_puntaje;
+    [SerializeField]
+    private int valor_gatito;
+    private ControlGatitos control_gatitos;
+
+    void Start()
+    {
+        control_gatitos = ui_puntaje.GetComponent<ControlGatitos>();
+    }
+    protected override void Pick(GameObject picker)
+    {
+        control_gatitos.ActualizarPuntaje(valor_gatito);
+    }
+}
